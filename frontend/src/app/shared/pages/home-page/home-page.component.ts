30e7166e4ac875c249014f3032bf22f9be13b774
BNSebastian <bucovala.sebastian@protonmail.com>
2024-05-03 23:39:22 +0300

    update

--- frontend/src/app/shared/pages/home-page/home-page.component.ts
+++ frontend/src/app/shared/pages/home-page/home-page.component.ts
@@ -5,6 +5,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 
 import { AuthService } from '../../../core/auth/services/auth.service';
+import { MathJaxParagraphComponent } from '../../components/math-jax-paragraph/math-jax-paragraph.component';
 
 @Component({
   selector: 'app-home-page',
@@ -15,9 +16,12 @@ import { AuthService } from '../../../core/auth/services/auth.service';
     MatExpansionModule,
     MatGridListModule,
     MatIconModule,
+    MathJaxParagraphComponent,
   ],
 })
 export class HomePageComponent {
   public panelOpenState = false;
   public authService = inject(AuthService);
+  public test: number = 3;
+  public firstEq: string = `Just testing how well a random equation \(${this.test} + y^2 = z^2\) in the middle of a sentence works.`;
 }
